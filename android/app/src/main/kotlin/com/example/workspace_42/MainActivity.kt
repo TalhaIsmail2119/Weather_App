2eb213c59f2fbcfa9ae828ca6b4706dc10efbe48
Talha Ismail <talha.ismail2119@gmail.com>
2024-01-07 20:38:51 +0600

    Weather App

---
+++ android/app/src/main/kotlin/com/example/workspace_42/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.workspace_42
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:jiffy/jiffy.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    position = await Geolocator.getCurrentPosition();
+    getWeatherData();
+    print(
+        "my latitude is ${position!.latitude}  longitute is ${position!.longitude}");
+  }
+
+  Position? position;
+
+  Map<String, dynamic>? weatherMap;
+  Map<String, dynamic>? forecastMap;
+
+  getWeatherData() async {
+    var weather = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?lat=${position!.latitude}&lon=${position!.longitude}&appid=cc93193086a048993d938d8583ede38a&units=metric"));
+    print("wweqwerqweqwewqeqwe ${weather.body}");
+    var forecast = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/forecast?lat=${position!.latitude}&lon=${position!.longitude}&appid=cc93193086a048993d938d8583ede38a&units=metric"));
+
+    var weatherData = jsonDecode(weather.body);
+    var forecastData = jsonDecode(forecast.body);
+
+    setState(() {
+      weatherMap = Map<String, dynamic>.from(weatherData);
+      forecastMap = Map<String, dynamic>.from(forecastData);
+    });
+  }
+
+  @override
+  void initState() {
+    determinePosition();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: weatherMap != null
+          ? Scaffold(
+              backgroundColor: Color.fromARGB(255, 166, 215, 238),
+              body: Container(
+                padding: EdgeInsets.all(25),
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                              "${Jiffy.parse("${DateTime.now()}").format(pattern: 'MMM do yyyy')}, ${Jiffy.parse("${DateTime.now()}").format(pattern: 'h:mm')}"),
+                          Text("${weatherMap!["name"]}"),
+                        ],
+                      ),
+                    ),
+                    Image.network(
+                        "https://openweathermap.org/img/wn/${weatherMap!["weather"][0]["icon"]}@2x.png"),
+                    Text("${weatherMap!["main"]["temp"]}°"),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          Text(
+                              "Feels Like ${weatherMap!["main"]["feels_like"]}"),
+                          Text("${weatherMap!["weather"][0]["description"]}"),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                        "Humidity ${weatherMap!["main"]["humidity"]},Pressure ${weatherMap!["main"]["pressure"]}"),
+                    Text(
+                        "Sunrise ${Jiffy.parse("${DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunrise"] * 1000)}").format(pattern: "hh mm a")}, Sunset  ${Jiffy.parse("${DateTime.fromMillisecondsSinceEpoch(weatherMap!["sys"]["sunset"] * 1000)}").format(pattern: "hh mm a")}"),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: 250,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: forecastMap!.length,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            //alignment: Alignment.center,
+                            color: Colors.blueGrey,
+                            width: 150,
+                            margin: EdgeInsets.only(right: 12),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                    "${Jiffy.parse("${forecastMap!["list"][index]["dt_txt"]}").format(pattern: "EEE h mm")}"),
+                                Image.network(
+                                    "https://openweathermap.org/img/wn/${forecastMap!["list"][index]["weather"][0]["icon"]}@2x.png"),
+                                Text(
+                                    "${forecastMap!["list"][index]["main"]["temp_min"]}"),
+                                Text(
+                                    "${forecastMap!["list"][index]["weather"][0]["description"]}"),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          : Container(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Center(child: CircularProgressIndicator()),
+            ),
+    );
+  }
+}
